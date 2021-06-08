d6c238ae73437f19ec446075190c4fc0a818aa9e
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 02:36:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function createRock(x) {
   }
   }
 
-   window.requestAnimationFrame(rock);
+   window.requestAnimationFrame(moveRock);
   // We should kick off the animation of the rock around here.
 
   // Add the rock to ROCKS so that we can remove all rocks
