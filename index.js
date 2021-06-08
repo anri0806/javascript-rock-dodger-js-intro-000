5e5e409469c7cc42845b8a8e5dcc46f3e7057a33
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 02:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval)
   ROCKS.forEach(function(rock){rock.remove()})
-  
+  moveDodger.delete();
 }
 
 function moveDodger(e) {
