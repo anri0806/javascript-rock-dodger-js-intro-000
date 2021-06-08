5b3ab298a658d511d22a725e141d4f06ba3463c2
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 03:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,13 +80,6 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
  if(e.which == 37) {
    moveDodgerLeft()
  } else if (e.which === 39) {
