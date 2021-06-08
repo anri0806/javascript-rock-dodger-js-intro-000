ff0a817858fefda27ab2e793123bd83901afdb50
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 03:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function moveDodgerRight() {
  window.requestAnimationFrame(function moveRight(){
    if (position < 360) {
    dodger.style.left = `${position + 4}px`}})
- }}
+ }
 
 /**
  * @param {string} p The position property
