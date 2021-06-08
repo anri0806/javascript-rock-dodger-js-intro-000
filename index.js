aa9a14fb601179b4554b7a24c8f99240e6c7812a
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 02:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,8 +44,7 @@ function createRock(x) {
 
   rock.className = 'rock';
   rock.style.left = `${x}px`;
-  rock.style.top = '0px';
-  var top = 0;
+  var top = rock.style.top = 0
 
   GAME.appendChild(rock);
 
