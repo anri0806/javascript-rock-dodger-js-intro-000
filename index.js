423789813274c08f04ef31ee8cd1a549b236f3be
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 03:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,7 +82,10 @@ function endGame() {
 function moveDodger(e) {
 var position = positionToInteger(dodger.style.left)
  if(e.which == 37) {
-   moveDodgerLeft()
+   if(position > LEFT_WALL) {
+     moveDodgerLeft();
+   }
+  
  } else if (e.which === 39) {
    moveDodgerRight()
  }
