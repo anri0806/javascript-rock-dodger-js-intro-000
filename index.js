52298aff068d226af1e7530bd8b5a129ef49f263
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 03:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+  window.requestAnimationFrame()
 }
 
 function moveDodgerRight() {
