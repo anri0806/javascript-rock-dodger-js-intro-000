bc67d0e5892c655ce297f85aebdf0f108b1ec7a2
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-08 03:59:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function moveDodger(e) {
 function moveDodgerLeft() {
  var position = positionToInteger(dodger.style.left)
  window.requestAnimationFrame(function moveLeft() {
-   dodger.style.left = `${position - 4}px}`})
+   DODGER.style.left = `${position - 4}px}`})
  }
 
 
